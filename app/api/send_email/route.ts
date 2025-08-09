76e4bbc3de79324d102697a10d5216e83e58ed85
jztchl <kmvishnu625@gmail.com>
2025-08-09 16:32:44 +0530

    dynamic form using resend sdk

--- app/api/send_email/route.ts
+++ app/api/send_email/route.ts
@@ -2,14 +2,16 @@ import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const my_email =process.env.MY_EMAIL as string;
+const from_email = process.env.FROM_EMAIL as string;
 
 export async function POST(request: Request) {
   try {
     const { name, email, subject, message } = await request.json();
 
     const data = await resend.emails.send({
-      from: 'onboarding@resend.dev', 
-      to: 'kmvishnu625@gmail.com',
+      from: from_email, 
+      to: my_email,
       subject,
       html: `
         <h3>New message from ${name} on Portfolio</h3>
