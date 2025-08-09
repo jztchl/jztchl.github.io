1436e23ff3e35de5264aa67864a7fb17490111ca
jztchl <kmvishnu625@gmail.com>
2025-08-09 21:47:11 +0530

    fixed mail issue

--- app/api/send_email/route.ts
+++ app/api/send_email/route.ts
@@ -4,7 +4,6 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 const my_email =process.env.MY_EMAIL as string;
 const from_email = process.env.FROM_EMAIL as string;
-
 export async function POST(request: Request) {
   try {
     const { name, email, subject, message } = await request.json();
@@ -12,7 +11,7 @@ export async function POST(request: Request) {
     const data = await resend.emails.send({
       from: from_email, 
       to: my_email,
-      subject,
+      subject: subject,
       html: `
         <h3>New message from ${name} on Portfolio</h3>
         <p><strong>Email:</strong> ${email}</p>
